30a9fe22e71b19797bd1b2bb26aea1ab23852af6
Valdis Guntis Bukalders <valdis.bukalders@giraffe360.com>
2024-01-18 21:19:08 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,147 @@
+use clap::Parser;
+use image::codecs::jpeg::JpegEncoder;
+use image::{self, ImageEncoder, Rgb};
+use kd_tree::KdTree;
+use rayon::prelude::*;
+use std::path::PathBuf;
+use std::time::{Duration, Instant};
+
+mod utils;
+
+use utils::{
+    process_point, spherical_to_cartesian, text_coords_to_spherical, CartesianCoordinate,
+    NearPointAlgorithm, OutputType,
+};
+
+use crate::utils::ProcessedPoint;
+
+// CLI Parameters
+// -----------
+#[derive(Parser, Debug)]
+#[command( version, about="This is a CLI tool for generating normal maps, given an equirectangular format depth map", long_about = None )]
+struct Args {
+    // Input depth map file path
+    #[arg(name = "Input file path (depth map)", short = 'i', long = "input")]
+    input_path: PathBuf,
+    // Output normal map file path
+    #[arg(name = "Output file path (normal map)", short = 'o', long = "output")]
+    output_path: PathBuf,
+    // How many nearest neighbors to consider
+    #[arg(
+        name = "Num of nearest neighbors to consider",
+        short = 'k',
+        long = "k-nearest",
+        default_value_t = 80usize
+    )]
+    k_nearest: usize,
+    // Whether to output normals or absolute normals
+    #[arg(
+        name = "Output color",
+        short = 'c',
+        long = "color",
+        default_value = "shaded"
+    )]
+    output_color: OutputType,
+    #[arg(
+        name = "Nearest point algorithm",
+        short = 'a',
+        long = "nearest_algorithm",
+        default_value = "within_radius"
+    )]
+    algorithm: NearPointAlgorithm,
+}
+
+fn main() {
+    let args = Args::parse();
+    if args.k_nearest <= 0 {
+        panic!("Number of nearest neighbors must be greater than 0");
+    }
+    // check if output directory exists?
+    let time_total_calc = Instant::now();
+    let time_start = Instant::now();
+    let img = image::open(args.input_path)
+        .expect("File not found!")
+        .into_luma16();
+
+    let (img_width, img_height) = img.dimensions();
+
+    let mut new_img = image::ImageBuffer::<image::Rgb<u8>, _>::new(img_width, img_height);
+
+    let num_points = img_width * img_height;
+
+    let mut coordinates = Vec::<CartesianCoordinate>::with_capacity((num_points) as usize);
+
+    for (x, y, pixel) in img.enumerate_pixels() {
+        let depth = pixel.0[0] as f32;
+
+        let spherical = text_coords_to_spherical(x, y, (img_width, img_height), depth);
+        let cartesian = spherical_to_cartesian(&spherical);
+
+        coordinates.push(cartesian);
+    }
+
+    let tree = KdTree::par_build_by_ordered_float(coordinates.clone());
+
+    let elapsed_tree_constr = time_start.elapsed();
+    println!("KD tree construction took {:.2?}", elapsed_tree_constr);
+
+    println!(
+        "Starting normal calculation using {:} nearest neighbors",
+        args.k_nearest
+    );
+
+    let results = coordinates
+        .par_iter()
+        .map(|point| {
+            process_point(
+                &point,
+                args.k_nearest,
+                &tree,
+                args.algorithm,
+                args.output_color,
+            )
+        })
+        .collect::<Vec<ProcessedPoint>>();
+
+    let mut elapsed_tree_search = Duration::from_secs(0);
+    let mut elapsed_plane_fit = Duration::from_secs(0);
+
+    let writer = std::fs::File::create(&args.output_path).expect("Failed to create file");
+    let encoder = JpegEncoder::new_with_quality(writer, 100);
+
+    let elapsed_calc = time_total_calc.elapsed();
+
+    for result_px in &results {
+        new_img.put_pixel(
+            result_px.text_coords.x,
+            result_px.text_coords.y,
+            Rgb::<u8>(result_px.rgb),
+        );
+        elapsed_tree_search += result_px.tree_search_elapsed;
+        elapsed_plane_fit += result_px.plane_fit_elapsed;
+    }
+
+    encoder
+        .write_image(&new_img, img_width, img_height, image::ColorType::Rgb8)
+        .expect("Error writing image");
+
+    elapsed_tree_search /= num_points;
+    elapsed_plane_fit /= num_points;
+
+    println!(
+        "Total calculation took {:.2?} with {:} nearest neighbors",
+        elapsed_calc, args.k_nearest
+    );
+    println!(
+        "Average time per pixel for tree search: {:.2?}",
+        elapsed_tree_search
+    );
+    println!(
+        "Average time per pixel for plane fit: {:.2?}",
+        elapsed_plane_fit
+    );
+    println!(
+        "File has been saved to {}",
+        args.output_path.to_str().unwrap()
+    );
+}
---
+++ src/utils.rs
@@ -0,0 +1,240 @@
+use clap::ValueEnum;
+use core::panic;
+use kd_tree::KdPoint;
+use nalgebra::{Const, Dyn, Matrix, SVector, VecStorage, SVD};
+use std::{
+    f32::consts::PI,
+    time::{Duration, Instant},
+};
+
+use kd_tree::KdTree;
+
+pub type Vec3 = SVector<f32, 3>;
+
+#[derive(Copy, Clone)]
+pub struct TextCoords {
+    pub x: u32,
+    pub y: u32,
+}
+
+pub struct SphericalCoordinate {
+    pub theta: f32,
+    pub phi: f32,
+    pub r: f32,
+    pub from_text: TextCoords,
+}
+
+#[derive(Copy, Clone)]
+pub struct CartesianCoordinate {
+    pub vec_coord: Vec3,
+    pub from_text: TextCoords,
+}
+
+impl KdPoint for CartesianCoordinate {
+    type Scalar = f32;
+    type Dim = typenum::U3;
+    fn at(&self, k: usize) -> f32 {
+        match k {
+            0 => self.vec_coord.x,
+            1 => self.vec_coord.y,
+            2 => self.vec_coord.z,
+            _ => panic!("Invalid dimension"),
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub enum OutputType {
+    Shaded,
+    Normals,
+}
+impl ValueEnum for OutputType {
+    fn from_str(input: &str, _ignore_case: bool) -> Result<Self, String> {
+        match input {
+            "shaded" => Ok(OutputType::Shaded),
+            "normals" => Ok(OutputType::Normals),
+            _ => Err(format!("Invalid output color: {}", input)),
+        }
+    }
+    fn to_possible_value(&self) -> Option<clap::builder::PossibleValue> {
+        match self {
+            OutputType::Shaded => Some(clap::builder::PossibleValue::new("shaded")),
+            OutputType::Normals => Some(clap::builder::PossibleValue::new("normals")),
+        }
+    }
+    fn value_variants<'a>() -> &'a [Self] {
+        &[OutputType::Shaded, OutputType::Normals]
+    }
+}
+#[derive(Copy, Clone, Debug)]
+pub enum NearPointAlgorithm {
+    KNearest,
+    WithinRadius,
+}
+impl ValueEnum for NearPointAlgorithm {
+    fn from_str(input: &str, _ignore_case: bool) -> Result<Self, String> {
+        match input {
+            "k_nearest" => Ok(NearPointAlgorithm::KNearest),
+            "within_radius" => Ok(NearPointAlgorithm::WithinRadius),
+            _ => Err(format!("Invalid algorithm: {}", input)),
+        }
+    }
+    fn to_possible_value(&self) -> Option<clap::builder::PossibleValue> {
+        match self {
+            NearPointAlgorithm::KNearest => Some(clap::builder::PossibleValue::new("k_nearest")),
+            NearPointAlgorithm::WithinRadius => {
+                Some(clap::builder::PossibleValue::new("within_radius"))
+            }
+        }
+    }
+    fn value_variants<'a>() -> &'a [Self] {
+        &[
+            NearPointAlgorithm::KNearest,
+            NearPointAlgorithm::WithinRadius,
+        ]
+    }
+}
+
+pub struct ProcessedPoint {
+    pub text_coords: TextCoords,
+    pub rgb: [u8; 3],
+    pub tree_search_elapsed: Duration,
+    pub plane_fit_elapsed: Duration,
+}
+
+pub fn text_coords_to_spherical(
+    x: u32,
+    y: u32,
+    dimensions: (u32, u32),
+    depth: f32,
+) -> SphericalCoordinate {
+    let perc_x = x as f32 / dimensions.0 as f32;
+    let perc_y = y as f32 / dimensions.1 as f32;
+    let clip_coords = (perc_x * 2. - 1., perc_y * 2. - 1.);
+    return SphericalCoordinate {
+        theta: clip_coords.0 as f32 * PI,
+        phi: clip_coords.1 as f32 * (PI / 2.),
+        r: depth,
+        from_text: TextCoords { x, y },
+    };
+}
+
+pub fn spherical_to_cartesian(sph: &SphericalCoordinate) -> CartesianCoordinate {
+    let adjusted_phi = sph.phi + PI / 2.;
+    let vec = Vec3::new(
+        sph.r * adjusted_phi.sin() * sph.theta.cos(),
+        sph.r * adjusted_phi.sin() * sph.theta.sin(),
+        sph.r * adjusted_phi.cos(),
+    );
+    return CartesianCoordinate {
+        vec_coord: vec,
+        from_text: sph.from_text,
+    };
+}
+
+pub fn process_point(
+    point: &CartesianCoordinate,
+    k: usize,
+    tree: &KdTree<CartesianCoordinate>,
+    algorithm: NearPointAlgorithm,
+    desired_output: OutputType,
+) -> ProcessedPoint {
+    let tree_search_start = Instant::now();
+
+    let k_nearest = match algorithm {
+        NearPointAlgorithm::KNearest => tree
+            .nearests(point, k)
+            .iter()
+            .map(|x| x.item)
+            .collect::<Vec<&CartesianCoordinate>>(),
+        NearPointAlgorithm::WithinRadius => {
+            let mut radius = k as f32;
+            let mut points = tree.within_radius(point, radius);
+            while points.len() < 10 {
+                radius *= 2.;
+                points = tree.within_radius(point, radius);
+            }
+            points
+        }
+    };
+
+    let tree_search_elapsed = tree_search_start.elapsed();
+    let num_points_sampled = k_nearest.len();
+
+    let plane_fit_start = Instant::now();
+    let mut sum = Vec3::new(0., 0., 0.);
+
+    for near_point in &k_nearest {
+        sum = sum + near_point.vec_coord;
+    }
+
+    // Calculate normal of the plane using SVD
+    // https://math.stackexchange.com/a/99317
+    let centroid = sum / num_points_sampled as f32;
+
+    // store the nearest points in a 3 x N matrix where N = number of points sampled
+    // and each column is a point
+    let matrix = Matrix::<f32, Const<3>, Dyn, VecStorage<f32, Const<3>, Dyn>>::from_fn(
+        num_points_sampled,
+        |row, col| {
+            if col >= k_nearest.len() {
+                panic!("Invalid column");
+            }
+            let point = k_nearest[col];
+            match row {
+                0 => point.vec_coord.x - centroid.x,
+                1 => point.vec_coord.y - centroid.y,
+                2 => point.vec_coord.z - centroid.z,
+                _ => panic!("Invalid row"),
+            }
+        },
+    );
+
+    let svd = SVD::new(matrix, true, false);
+
+    let plane_fit_elapsed = plane_fit_start.elapsed();
+
+    let left_singular = svd.u.unwrap();
+
+    let normal_col = left_singular.column(2);
+
+    let mut normal_full_vec = normal_col.normalize();
+
+    if normal_full_vec.dot(&point.vec_coord) < 0. {
+        // If normal is oriented away from the camera, flip it so it's facing the camera
+        normal_full_vec *= -1.;
+    }
+
+    const LIGHT_POS: Vec3 = Vec3::new(0., -3000., 0.);
+
+    let light_dir = point.vec_coord - LIGHT_POS;
+
+    let dist_to_light = (light_dir.magnitude() / (256. * 40.)).min(1.);
+
+    let light_level = normal_full_vec.dot(&light_dir.normalize()).max(0.) * (1. - dist_to_light);
+
+    let shaded_rgb = [
+        (light_level * 255.) as u8,
+        (light_level * 255.) as u8,
+        (light_level * 255.) as u8,
+    ];
+
+    let normal_abs_vec = normal_col.abs().normalize();
+
+    let normal_abs_rgb = [
+        (normal_abs_vec.x * 255.) as u8,
+        (normal_abs_vec.y * 255.) as u8,
+        (normal_abs_vec.z * 255.) as u8,
+    ];
+
+    let text_coords = point.from_text;
+    return ProcessedPoint {
+        text_coords,
+        rgb: match desired_output {
+            OutputType::Shaded => shaded_rgb,
+            OutputType::Normals => normal_abs_rgb,
+        },
+        tree_search_elapsed,
+        plane_fit_elapsed,
+    };
+}
